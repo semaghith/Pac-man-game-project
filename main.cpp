97fbccaee0a970de763ed605079b3a5e9d9365f1
sema ghith <62404758+semaghith@users.noreply.github.com>
2020-03-26 11:36:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,41 +2,41 @@
 #include <SFML/Audio.hpp>
 
 using namespace sf;
-int main() {
 
-    RenderWindow window(VideoMode(620, 700), " ");
+int main(){   
+    RenderWindow window(VideoMode(620,700)," ") ;
 
-    RectangleShape player(Vector2f(620, 700));               // INTRO RECT. IMAGE
+    RectangleShape player(Vector2f(620, 700));            // INTRO RECT. IMAGE
 
-    Texture image1;                                         // INTRO IMAGE
+    Texture image1;                                      //INTRO IMAGE
     image1.loadFromFile("data/PAC-MAN-Game.jpg");
     player.setTexture(&image1);
-
-
+   
+    
     Font font;
-   font.loadFromFile("data/arial.ttf");
-       
-    Text start;                                            //INTRO TEXT
+    if (font.loadFromFile("data/arial.ttf") == 0) {
+        return 0;
+    }
+
+    Text start;                                             //INTRO TEXT
     start.setFont(font);
     start.setCharacterSize(30);
     start.setString("TAP TO START!");
     start.setFillColor(Color::Yellow);
     start.setPosition(200, 450);
-
-
-    while (window.isOpen()) {                             // GAME LOOP
+    
+    while (window.isOpen()) {                               // GAME LOOP
         Event event;
         while (window.pollEvent(event)) {
-            switch (event.type) {
-            case Event::Closed: window.close();
-                break;
-
-            }
+           if (event.type == Event::Closed ) {
+               window.close();
+           }
         }
+       
         window.clear();
         window.draw(player);
         window.draw(start);
-        window.display();
+        window.display() ;
     }
     return 0;
 }
